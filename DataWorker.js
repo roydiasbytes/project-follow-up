36238643847b7692a5afb0ea69fd14e76ded276a
Roy Van den Heede <roy@diasbytes.com>
2019-05-24 12:40:19 +0200

    adding data.json to app files and importing projects

---
+++ DataWorker.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const path = require('path');
+const {app} = require('electron');
+const util = require('util');
+const writeFile = util.promisify(fs.writeFile);
+const readFile = util.promisify(fs.readFile);
+class DataWorker{
+
+
+
+    static loadData(){
+        return new Promise(async(resolve,reject)=>{
+            try{
+                const dataPath = DataWorker.getDataPath();
+                if(!fs.existsSync(dataPath)){
+                    await DataWorker.writeData(DataWorker.getInitData());
+                    resolve({});
+                }
+                else{
+                    let buffer = await readFile(dataPath);
+                    resolve(JSON.parse(buffer.toString()));
+                }
+            } catch(error){
+                reject(error);
+            }
+        });
+    }
+
+    static writeData(data){
+        return new Promise(async(resolve,reject)=>{
+            try{
+                const dataPath = DataWorker.getDataPath();
+                await writeFile(dataPath,JSON.stringify(data));
+                resolve();
+            } catch(error){
+                reject(error);
+            }
+        });
+    }
+
+    static getDataPath(){
+        return path.join(app.getPath('appData'),'project-follow-up','data.json');
+    }
+
+    static getInitData(){
+        const data = {
+            projects:[
+                {
+                name:"Demo Project",
+                customer:"DiASBytes BVBA",
+                deadline:"12-19",
+                status: "new",
+                type:"new business",
+                notes:"new project to get us going"
+                }
+            ]
+        };
+        return data;
+    }
+
+
+}
+module.exports = DataWorker;
--- renderer.js
+++ renderer.js
@@ -1,3 +1,24 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const {ipcRenderer} = require('electron');
+
+// say that I'm done
+ipcRenderer.send("init-ready");
+
+// update data coming from main
+ipcRenderer.on('data-update',(event,data)=>{
+    let tbody = document.getElementById('projects-table-body');
+    let html = "";
+    for(var i = 0; i < data.projects.length; i++){
+        const project = data.projects[i];
+        html += `<tr><td>${project.name}</td><td>${project.customer}</td><td>${project.deadline}</td><td>${project.status}</td><td>${project.type}</td></tr>`;
+    }
+    tbody.innerHTML = html;
+});
+
+// define btn-new-project
+const btnNew = document.getElementById('btn-new-project');
+btnNew.addEventListener('click',()=>{
+    // open new window to add project
+});
